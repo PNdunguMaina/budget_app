d64ce0f818d4ab792edaa313de63918e9597dde9
PNdunguMaina <ndunguwamaina@gmail.com>
2023-02-01 16:16:49 +0300

    Create controller files with scaffold_controller command

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :groups do
+    resources :payments
+  end
+ 
+  
   devise_for :users
   get 'render/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
