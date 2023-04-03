39ce41abc3d8c3feb8073ba3e95242d3953c0e4b
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-03 14:04:45 +0300

    Update Sprockets configuration settings

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,5 +19,7 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.js_compressor = :uglifier
   end
 end
