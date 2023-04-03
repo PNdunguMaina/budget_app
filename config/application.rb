94dbee6ac3736f59598b3f7761d8bb61888852f4
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-03 14:53:48 +0300

    Add 'terser' library to the project

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.assets.js_compressor = :uglifier
   end
 end
