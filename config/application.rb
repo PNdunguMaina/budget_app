504b2f1217738ee6d4a0433d8f573bc440be6345
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-03 14:56:05 +0300

    Change compressor from terser to Uglifier

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.js_compressor = :uglifier
   end
 end
